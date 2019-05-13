7facf7bfa553fd2b6af4ee9a9b0809d0c6abe0b2
Oliver-Strange <preston.burton@gmail.com>
2019-05-13 15:27:57 -0700

    passed addToSaveList function to savedList.js, changed the saved movie into navLink that goes back to movie

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
@@ -14,7 +14,7 @@ export default class App extends Component {
     };
   }
 
-  addToSavedList = movie => {
+  addToSavedList = (movie) => {
     const savedList = this.state.savedList;
     savedList.push(movie);
     this.setState({ savedList });
@@ -25,7 +25,9 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path='/' component={MovieList} />
-        <Route path='/movies/:id' component={Movie} />
+        <Route path='/movies/:id' render={ (props) => {
+          return(<Movie {...props} addToSavedList={this.addToSavedList}/>)
+        }} />
       </div>
     );
   }
