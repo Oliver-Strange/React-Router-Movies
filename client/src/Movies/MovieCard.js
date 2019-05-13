463a2cadb4651b99844fe4b65f6a158c7a8c0110
Oliver-Strange <preston.burton@gmail.com>
2019-05-13 14:57:47 -0700

    refactored code with movie card taking all the data and passing as a component to movie and movie list

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 
-const MovieCard = props => {
-  // console.log(props);
-  // const id = props.match.params.id
-  // const movie = movies.find(movie => {
-  //   return `${movie.id}` === id})
+const MovieCard = (props) => {
+  const { title, director, metascore, stars } = props.movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieCard;
